af6905dc6a1f08efebad236cf0b50d0e5f12ac68
vvvalero <vvaleroj6@gmail.com>
2025-11-07 11:43:25 +0100

    Cambio titulo

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $resultadoMoviles = $conexion->query($sqlMoviles);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nevom - Tu Tienda de Móviles de Confianza</title>
+    <title>NEVOM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
